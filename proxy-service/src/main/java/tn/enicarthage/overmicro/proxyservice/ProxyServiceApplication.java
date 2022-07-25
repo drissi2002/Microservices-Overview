e48818ec261013c5c0119aa18477d28486a9c473
drissi houcem eddine <84160502+drissi2002@users.noreply.github.com>
2022-07-25 23:45:13 +0100

    Add files via upload

---
+++ proxy-service/src/main/java/tn/enicarthage/overmicro/proxyservice/ProxyServiceApplication.java
@@ -0,0 +1,17 @@
+package tn.enicarthage.overmicro.proxyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ProxyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+
+}
